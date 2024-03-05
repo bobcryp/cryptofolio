9310b587e8877cd72a745111652d6878d21a61d3
Thomas Chauve <thomas.chauve@univ-grenoble-alpes.fr>
2024-03-05 09:26:46 +0100

    add crypto in hard

--- lib_cryptofolio/get_price.py
+++ lib_cryptofolio/get_price.py
@@ -27,7 +27,15 @@ def coingecko_data(it=1,file_path=None):
                 data = data + json.loads(result.stdout)
             except subprocess.CalledProcessError as e:
                 print("Error executing curl command:", e)
-
+        # add crypto not found
+        curl_command = [
+                'curl',
+                '-X', 'GET',        
+                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=orion-protocol%2Cdigibyte%2Cswissborg&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d&locale=en'
+        ]
+        result = subprocess.run(curl_command, capture_output=True, text=True, check=True)
+        data = data + json.loads(result.stdout)
+        
     return data
 
 def eurusd_change():
